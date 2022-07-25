c79252ab92feb38934ca2117233b84ecee2e7bd3
Sanket Shankar Manne <104842361+Sanketmanne@users.noreply.github.com>
2022-07-26 02:18:23 +0530

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,31 @@
+function moreaboutme() {
+    var x = document.getElementById('myDIV');
+    var y = document.getElementById('welcome-section');
+    if (x.style.display == "block") {
+      x.style.display = "none";
+       y.style.height = "90rem";
+     
+    } else {
+      x.style.display = "block";
+       y.style.height = "140rem";
+    }
+  }
+  
+  
+  function darkmode() {
+  
+  if(document.body.style.backgroundColor == "white")
+  {
+    document.getElementById("darkmodebutton").childNodes[0].nodeValue=
+     "Normal Mode";
+    document.body.style.backgroundColor = "#000000cf";
+    document.body.style.color = "white";
+    }
+    else
+    {
+        document.getElementById("darkmodebutton").childNodes[0].nodeValue=
+     "Dark Mode";
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "#000000cf";
+    }
+  }
